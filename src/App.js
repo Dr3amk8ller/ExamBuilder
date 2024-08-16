353dd8699d5316a0a950ce2a0f411e810ca5717f
Prince <princemeghani100@gmail.com>
2024-08-16 14:25:20 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,39 +1,49 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'; 
+import HomePage from './Dashboard/HomePage';
 import Login from './Login';
-import AboutUs from './Components/AboutUs';
 import Register from './UserAuth/Register';
 import Navigation from './Navigation'; 
 import NavigationBar from './Dashboard/NavigationBar'; 
-import HomePage from './Dashboard/HomePage';
-import Header from './Header';
-import Homepage from './HomePrev';
 import AboutUsPage from './AboutUsPage';
 import FeaturesPage from './FeaturesPage';
 import HelpPage from './HelpPage';
-
-
+import Header from './Header';
 
 const App = () => {
-  return (
+  const hostname = window.location.hostname;
 
+  return (
     <Router>
       <div className="App">
-   
         <Routes>
-        <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register/>} />
-          {/* <Route path="/about" element={<AboutUs />} /> */}
-          <Route path="/navigation/*" element={<Navigation />} />
-          <Route path="/NavigationBar" element={<NavigationBar />} /> 
-          <Route path="/header" element={<Header />} /> 
-          {/* <Route path="/HomePage" element={<HomePage />} />  */}
-          <Route path="/Aboutpage" element={<AboutUsPage />} /> 
-          <Route path="/Features" element={<FeaturesPage />} /> 
-          <Route path="/Helppage" element={<HelpPage />} /> 
-            
-         
+          {hostname === 'exambuilder.online' ? (
+            <>
+              <Route path="/" element={<HomePage />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </>
+          ) : hostname === 'admin.exambuilder.online' ? (
+            <>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/navigation/*" element={<Navigation />} />
+              <Route path="/NavigationBar" element={<NavigationBar />} />
+              <Route path="*" element={<Navigate to="/login" />} />
+            </>
+          ) : (
+            <>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/navigation/*" element={<Navigation />} />
+              <Route path="/NavigationBar" element={<NavigationBar />} /> 
+              <Route path="/header" element={<Header />} /> 
+              <Route path="/Aboutpage" element={<AboutUsPage />} /> 
+              <Route path="/Features" element={<FeaturesPage />} /> 
+              <Route path="/Helppage" element={<HelpPage />} /> 
+              <Route path="*" element={<Navigate to="/" />} />
+            </>
+          )}
         </Routes>
       </div>
     </Router>
